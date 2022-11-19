eaa7ef980e6833cd171f2269c2502573c8de182d
Victor Ivan <victor.simoes@brmalls.com.br>
2022-11-19 14:01:49 -0300

    renaming some functions

--- src/utils/get_line_from_terminal.c
+++ src/utils/get_line_from_terminal.c
@@ -6,13 +6,13 @@
 /*   By: vivan-de <vivan-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 10:54:35 by vivan-de          #+#    #+#             */
-/*   Updated: 2022/11/19 13:55:42 by vivan-de         ###   ########.fr       */
+/*   Updated: 2022/11/19 14:00:51 by vivan-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-char	*get_line(void)
+char	*get_line_from_terminal(void)
 {
 	return (readline(COLOR_BOLD GRN "minishell $> " RESET COLOR_OFF));
 }
