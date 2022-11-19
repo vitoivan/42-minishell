c282ce8548eba1021a987c083010753bc3141a6d
Victor Ivan <victor.simoes@brmalls.com.br>
2022-11-19 13:58:18 -0300

    feat: adding some definitions and colors

--- includes/define.h
+++ includes/define.h
@@ -1,21 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   define.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vivan-de <vivan-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/19 15:21:11 by victor            #+#    #+#             */
-/*   Updated: 2022/11/19 13:55:13 by vivan-de         ###   ########.fr       */
+/*   Updated: 2022/11/19 13:49:57 by vivan-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#ifndef DEFINE_H
+# define DEFINE_H
 
-# include "../libs/libft/libft.h"
-# include "./define.h"
-# include "./utils.h"
-# include <readline/readline.h>
+# define BOOL int
+
+enum	e_bool
+{
+	False = 0,
+	True = 1
+};
+
+# define RED "\x1B[31m"
+# define GRN "\x1B[32m"
+# define YEL "\x1B[33m"
+# define BLU "\x1B[34m"
+# define MAG "\x1B[35m"
+# define CYN "\x1B[36m"
+# define WHT "\x1B[37m"
+# define RESET "\x1B[0m"
+# define COLOR_BOLD "\e[1m"
+# define COLOR_OFF "\e[m"
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: vivan-de <vivan-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 10:54:35 by vivan-de          #+#    #+#             */
-/*   Updated: 2022/11/19 10:55:15 by vivan-de         ###   ########.fr       */
+/*   Updated: 2022/11/19 13:57:39 by vivan-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,18 @@
 
 int	main(void)
 {
-	ft_printf("Boiler plate\n");
+	char	*line;
+
+	while (True)
+	{
+		line = get_line();
+		if (!strcmp(line, "exit"))
+		{
+			free(line);
+			return (0);
+		}
+		ft_printf("line: %s\n", line);
+		free(line);
+	}
 	return (0);
 }
