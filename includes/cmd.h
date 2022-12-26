b9dec4173c8cf6b03a6b32561c1fc6917ed3b5be
Victor Ivan <victor.simoes@brmalls.com.br>
2022-12-26 06:55:05 -0300

    feat: cmd_is_valid

--- includes/cmd.h
+++ includes/cmd.h
@@ -1,6 +1,8 @@
 #ifndef CMD_H
 # define CMD_H
 
+# include "./define.h"
+
 typedef struct s_cmd
 {
 	char	*cmd;
@@ -8,6 +10,8 @@ typedef struct s_cmd
 }			t_cmd;
 
 t_cmd		*cmd_new(char *cmd, char **args);
-char		*get_binary_path(char *cmd);
+char		*get_cmd_binary_path(char *cmd);
+char		**get_path(void);
+BOOL		cmd_is_valid(char *cmd);
 
 #endif
