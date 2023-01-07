4693b926d2fd5a1e0cbcc193de54330cae389ba2
Victor Ivan <victor.simoes@brmalls.com.br>
2023-01-07 10:45:07 -0300

    feat: using "~" to print /home/$USERNAME in terminal

--- src/context/init.c
+++ src/context/init.c
@@ -6,7 +6,7 @@
 /*   By: vivan-de <vivan-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 13:53:32 by vivan-de          #+#    #+#             */
-/*   Updated: 2022/12/26 16:14:24 by vivan-de         ###   ########.fr       */
+/*   Updated: 2022/12/31 08:28:18 by vivan-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_ctx	*ctx_init(void)
 	t_ctx	*ctx;
 
 	ctx = (t_ctx *)ft_calloc(1, sizeof(t_ctx));
-	ctx->user = getenv("USER");
+	ctx->user = getenv("USERNAME");
 	ctx->path = getcwd(ctx->path, PATH_SIZE);
 	ctx->hostname = get_hostname();
 	return (ctx);
--- src/utils/print_terminal.c
+++ src/utils/print_terminal.c
@@ -6,13 +6,43 @@
 /*   By: vivan-de <vivan-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 10:54:35 by vivan-de          #+#    #+#             */
-/*   Updated: 2022/12/27 09:19:36 by vivan-de         ###   ########.fr       */
+/*   Updated: 2022/12/31 08:34:50 by vivan-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	print_terminal(t_ctx *ctx)
+static char	*get_path_fmt(t_ctx *ctx)
+{
+	char	**splitted;
+	int		skip_n_home;
+	char	*path_fmt;
+	int		i;
+
+	i = 0;
+	splitted = ft_split(ctx->path, '/');
+	if (!ft_strcmp(splitted[0], "home") && !ft_strcmp(splitted[1], ctx->user))
+	{
+		skip_n_home = ft_strlen(splitted[0]) + ft_strlen(splitted[1]) + 2;
+		path_fmt = ft_strjoin("~", ctx->path + skip_n_home);
+	}
+	else
+		path_fmt = ft_strdup(ctx->path);
+	while (splitted[i])
+		free(splitted[i++]);
+	free(splitted);
+	return (path_fmt);
+}
+
+static const char	*get_printf_str(void)
+{
+	char	*s;
+
+	s = COLOR_BOLD GREEN "%s" WHT ":" BLU "%s" WHT "$ " RESET_COLOR REMOVE_BOLD;
+	return ((const char *)s);
+}
+
+static char	*get_user_and_host(t_ctx *ctx)
 {
 	char	*user_and_host;
 	char	*tmp;
@@ -20,8 +50,17 @@ void	print_terminal(t_ctx *ctx)
 	tmp = ft_strjoin(ctx->user, "@");
 	user_and_host = ft_strjoin(tmp, ctx->hostname);
 	free(tmp);
-	ft_printf(COLOR_BOLD GREEN "%s" WHT ":" BLU "%s" WHT "$ " RESET_COLOR REMOVE_BOLD,
-				user_and_host,
-				ctx->path);
-	free(user_and_host);
+	return (user_and_host);
+}
+
+void	print_terminal(t_ctx *ctx)
+{
+	char	*user_and_host;
+	char	*path;
+
+	user_and_host = get_user_and_host(ctx);
+	path = get_path_fmt(ctx);
+	ft_printf(get_printf_str(), user_and_host, path);
+	free_if_exists((void **)&path);
+	free_if_exists((void **)&user_and_host);
 }
